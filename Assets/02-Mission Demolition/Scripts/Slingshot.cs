62b4f0f66a8508a5c758deff7cc70df4fafe9be4
Junyaoooo <123599025+Junyaoooo@users.noreply.github.com>
2023-02-08 15:21:29 -0500

    onMouseEnter text

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseEnter() 
+    {
+        print("Slingshot:OnMouseEnter()");
+    }
+
+    void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+    }
+}
