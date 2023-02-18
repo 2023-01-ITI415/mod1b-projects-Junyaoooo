8d1cceea2b8b2c59d6979ffc4b62bc326f18df19
Junyaoooo <123599025+Junyaoooo@users.noreply.github.com>
2023-02-18 16:40:35 -0500

    fish control ok, then let background up into Hierarchy

--- Assets/03-Prototype1/scripts/FishControl.cs
+++ Assets/03-Prototype1/scripts/FishControl.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class FishControl : MonoBehaviour
 {
+
+    //Fish Direction
+    public enum FishDirection
+    {
+        Left,
+        Right
+    }
+
+
+    //fish blood
+    private float hp;
+    //fish speed
+    private float speed1=5f;
+    //fish end point
+    private Vector3 target1;
+    //now fish Direction
+    private FishDirection currentDir1 = FishDirection.Left;
+
+
+
+    //set the end point for fish
+    //when fish left side end than go right
+    //when fish right side end than go left
+    private void SetTarget() 
+    {
+        int scalX1 = currentDir1 == FishDirection.Left ? -1 : 1;
+        transform.localScale=new Vector3(scalX1, 1, 1);
+
+        //random y from 0 to 1:
+        float randomY = Random.value;
+        //fish head setting
+        if (currentDir1 == FishDirection.Left)
+        {
+            //control fish swim left side
+            target1 = Camera.main.ViewportToWorldPoint(new Vector3 (0,randomY,-Camera.main.transform.position.z));
+        }
+        else 
+        {
+            //control fish swim right side
+            target1 = Camera.main.ViewportToWorldPoint(new Vector3(1, randomY, -Camera.main.transform.position.z));
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetTarget();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = Vector3.MoveTowards(transform.position,target1,Time.deltaTime*speed1);
+        transform.position = pos;
+
+        //if fish get the end point, if get, then set another new end point:
+        if (Vector3.Distance(transform.position,target1)<0.06f) 
+        {
+            //if fish and end point distance less than 0.06, then think that they equal
+            currentDir1= currentDir1==FishDirection.Left?FishDirection.Right:FishDirection.Left;
+            //end point
+            SetTarget();
+        }
     }
 }
