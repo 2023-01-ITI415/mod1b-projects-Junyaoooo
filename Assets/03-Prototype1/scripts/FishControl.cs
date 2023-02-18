3a34952d39ff2eb50c58a7319a4a50b4bd2f9e6a
Junyaoooo <123599025+Junyaoooo@users.noreply.github.com>
2023-02-18 18:38:28 -0500

    fishcontrol to create fish and every fish has different speed

--- Assets/03-Prototype1/scripts/FishControl.cs
+++ Assets/03-Prototype1/scripts/FishControl.cs
@@ -20,22 +20,30 @@ public class FishControl : MonoBehaviour
     //fish end point
     private Vector3 target1;
     //now fish Direction
-    private FishDirection currentDir1 = FishDirection.Left;
+    public FishDirection currentDir = FishDirection.Left;
+
+    public float Speed 
+    {
+        set { speed1 = value; }
+    }
 
 
-
+    public FishDirection CurrentDir 
+    {
+        set { CurrentDir = value; }
+    }
     //set the end point for fish
     //when fish left side end than go right
     //when fish right side end than go left
     private void SetTarget() 
     {
-        int scalX1 = currentDir1 == FishDirection.Left ? -1 : 1;
+        int scalX1 = currentDir == FishDirection.Left ? -1 : 1;
         transform.localScale=new Vector3(scalX1, 1, 1);
 
         //random y from 0 to 1:
         float randomY = Random.value;
         //fish head setting
-        if (currentDir1 == FishDirection.Left)
+        if (currentDir == FishDirection.Left)
         {
             //control fish swim left side
             target1 = Camera.main.ViewportToWorldPoint(new Vector3 (0,randomY,-Camera.main.transform.position.z));
@@ -64,7 +72,7 @@ public class FishControl : MonoBehaviour
         if (Vector3.Distance(transform.position,target1)<0.06f) 
         {
             //if fish and end point distance less than 0.06, then think that they equal
-            currentDir1= currentDir1==FishDirection.Left?FishDirection.Right:FishDirection.Left;
+            currentDir = currentDir == FishDirection.Left?FishDirection.Right:FishDirection.Left;
             //end point
             SetTarget();
         }
